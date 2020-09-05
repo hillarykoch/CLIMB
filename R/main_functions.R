fbc4baa7e1e08a44fd772d060d4cd4501e5a4713
hillarykoch <hillary.koch01@gmail.com>
2020-09-05 13:56:48 -0400

    update rconstr0_GMM

--- R/main_functions.R
+++ R/main_functions.R
@@ -6,118 +6,128 @@ rGMM <- function(n, prop, mu, sigma) {
     # d: dimension of data (number of replicates)
     # mu: list of mean vectors of clusters
     # sigma: list of variance covariance matrices of clusters
-
+    
     prop <- prop / sum(prop)
     k <- length(prop)
     d <- ifelse(length(prop) == 1, nrow(sigma), nrow(sigma[[1]]))
-    rles <- sample(seq_along(prop), n, replace = TRUE, prob = prop) %>%
+    rles <-
+        sample(seq_along(prop), n, replace = TRUE, prob = prop) %>%
         sort %>%
         rle
     num <- rep(0, k)
     num[rles$values] <- rles$lengths
-
+    
     tag <- rep(1:k, times = num)
     y <- lapply(seq_along(prop),
                 function(X)
-                    if(num[X] > 0) {
-                        rmvnorm(num[X], mean = mu[[X]], sigma = sigma[[X]])    
+                    if (num[X] > 0) {
+                        rmvnorm(num[X], mean = mu[[X]], sigma = sigma[[X]])
                     }) %>%
         abind(along = 1) %>%
         data.frame %>%
         setNames(sapply(seq(d), function(X)
             paste0("y.", X)))
-
-    list(
-        "data" = y,
-        "cluster" = tag
-    )
+    
+    list("data" = y,
+         "cluster" = tag)
 }
 
 # generate data for constrained version of GMCM
-rconstr_GMCM <- function(n, prop, mu, sigma, rho, d, combos = NULL) {
-    # n: sample size
-    # prop: mixing proportion of each cluster
-    # d: dimension of data (number of replicates)
-    # mu: mean of "reproducible" components
-    # sigma: variance of "reproducible" components
-    # rho: correlation between replicates of same component
-    # d: number of replicates
-    # k: number of clusters
-
-    if (is.null(combos)) {
-        # Generate all combinations of replication, given in any order
-        combos <- expand.grid(rep(list(-1:1), d)) %>% as.matrix
+rconstr_GMCM <-
+    function(n,
+             prop,
+             mu,
+             sigma,
+             rho,
+             d = NULL,
+             combos = NULL) {
+        # n: sample size
+        # prop: mixing proportion of each cluster
+        # d: dimension of data (number of replicates)
+        # mu: mean of "reproducible" components
+        # sigma: variance of "reproducible" components
+        # rho: correlation between replicates of same component
+        # d: number of replicates
+        # k: number of clusters
+        
+        if (is.null(combos)) {
+            # Generate all combinations of replication, given in any order
+            combos <- expand.grid(rep(list(-1:1), d)) %>% as.matrix
+        }
+        
+        k <- nrow(combos)
+        if (k != length(prop)) {
+            stop("length(prop) must be equal to total number of clusters.")
+        }
+        
+        sigma <- c(sigma, 1, sigma)
+        mu <- c(-mu, 0, mu)
+        prop <- prop / sum(prop)
+        
+        if (sum(prop == 0) > 0) {
+            keepidx <- prop != 0
+            combos <- combos[keepidx,]
+            prop <- prop[keepidx]
+            k <- sum(keepidx)
+        }
+        
+        rles <-
+            sample(seq_along(prop), n, replace = TRUE, prob = prop) %>%
+            sort %>%
+            rle
+        num <- rep(0, k)
+        num[rles$values] <- rles$lengths
+        tag <- rep.int(1:k, times = num)
+        
+        y <- lapply(seq_along(prop),
+                    function(X)
+                        if (num[X] > 0) {
+                            rmvnorm(num[X],
+                                    mean = mu[combos[X,] + 2],
+                                    sigma = get_constr_sigma(diag(sigma[combos[X,] +
+                                                                            2]),
+                                                             rho,
+                                                             combos[X,]))
+                        }) %>%
+            abind(along = 1) %>%
+            data.frame %>%
+            setNames(sapply(seq(d), function(X)
+                paste0("y.", X)))
+        
+        list("data" = y,
+             "cluster" = tag)
     }
-
-    k <- nrow(combos)
-    if (k != length(prop)) {
-        stop("length(prop) must be equal to total number of clusters.")
-    }
-
-    sigma <- c(sigma, 1, sigma)
-    mu <- c(-mu, 0, mu)
-    prop <- prop / sum(prop)
-
-    if (sum(prop == 0) > 0) {
-        keepidx <- prop != 0
-        combos <- combos[keepidx, ]
-        prop <- prop[keepidx]
-        k <- sum(keepidx)
-    }
-
-    rles <- sample(seq_along(prop), n, replace = TRUE, prob = prop) %>%
-        sort %>%
-        rle
-    num <- rep(0, k)
-    num[rles$values] <- rles$lengths
-    tag <- rep.int(1:k, times = num)
-
-    y <- lapply(seq_along(prop),
-                function(X)
-                    if(num[X] > 0) {
-                        rmvnorm(
-                            num[X],
-                            mean = mu[combos[X, ] + 2],
-                            sigma = get_constr_sigma(diag(sigma[combos[X, ] +
-                                                                    2]),
-                                                     rho,
-                                                     combos[X, ])
-                    )}) %>%
-        abind(along = 1) %>%
-        data.frame %>%
-        setNames(sapply(seq(d), function(X)
-            paste0("y.", X)))
-
-    list(
-        "data" = y,
-        "cluster" = tag
-    )
-}
 
 
 r_assoc <- function(d, num_patterns) {
     combos <- matrix(rep(NA, d * num_patterns),
                      nrow = num_patterns,
                      ncol = d)
-
+    
     # Always have the all 0 pattern
-    combos[1, ] <- rep(0, d)
-
+    combos[1,] <- rep(0, d)
+    
     count <- 2
-    while (any(is.na(combos[num_patterns, ]))) {
+    while (any(is.na(combos[num_patterns,]))) {
         assoc <- sample(-1:1, replace = TRUE, size = d)
         if (!any(apply(combos, 1, function(X)
             all(X == assoc)), na.rm = TRUE)) {
-            combos[count,] <- assoc
+            combos[count, ] <- assoc
             count <- count + 1
         }
     }
-  combos
+    combos
 }
 
 # generate data for second constrained version of GMCM
 rconstr0_GMM <-
-    function(n, prop, mu, sigma, rho, d, combos = NULL) {
+    function(n,
+             prop,
+             mu,
+             sigma,
+             rho,
+             d = NULL,
+             combos = NULL) {
         # n: sample size
         # prop: mixing proportion of each cluster
         # d: dimension of data (number of replicates)
@@ -129,6 +139,9 @@ rconstr0_GMM <-
         #        a 3-vector for negative, cross, positive association
         # d: number of replicates
         # k: number of clusters
+        if (is.null(d) & is.null(combos)) {
+            stop("At least one of combos or d needs to be specified.")
+        }
         
         # Generate all combinations of replication, given in any order
         if (is.null(combos)) {
@@ -158,6 +171,9 @@ rconstr0_GMM <-
         if (rho[1] < 0 | rho[3] < 0 | rho[2] > 0) {
             stop("rho[1] and rho[3] must be >= 0, rho[2] must be <= 0.")
         }
+        if (is.null(d)) {
+            d <- ncol(combos)
+        }
         
         sigma <- c(sigma[1], 1, sigma[2])
         mu <- c(mu[1], 0, mu[2])
@@ -180,12 +196,13 @@ rconstr0_GMM <-
         
         if (sum(prop == 0) > 0) {
             keepidx <- prop != 0
-            combos <- combos[keepidx, ]
+            combos <- combos[keepidx,]
             prop <- prop[keepidx]
             k <- sum(keepidx)
         }
         
-        rles <- sample(seq_along(prop), n, replace = TRUE, prob = prop) %>%
+        rles <-
+            sample(seq_along(prop), n, replace = TRUE, prob = prop) %>%
             sort %>%
             rle
         num <- rep(0, k)
@@ -194,23 +211,29 @@ rconstr0_GMM <-
         
         y <- lapply(seq_along(prop),
                     function(X)
-                        if(num[X] > 0) {
-                            rmvnorm(
-                                num[X],
-                                mean = mu_combos[X, ],
-                                sigma =
-                                    get_constr0_sigma(sig_combos[X,],
-                                                      combos[X,],
-                                                      rho)
-                            )}) %>%
+                        if (num[X] > 0) {
+                            rmvnorm(num[X],
+                                    mean = mu_combos[X,],
+                                    sigma =
+                                        get_constr0_sigma(sig_combos[X, ],
+                                                          combos[X, ],
+                                                          rho))
+                        }) %>%
             abind(along = 1) %>%
             data.frame %>%
             setNames(sapply(seq(d), function(X)
                 paste0("y.", X)))
         
-       
+        
         list(
             "data" = y,
-            "cluster" = tag
+            "cluster" = tag,
+            "params" =
+                list(
+                    "mu" = mu_combos,
+                    "Sigma" = simplify2array(lapply(
+                        seq_along(prop), function(X)
+                            get_constr0_sigma(sig_combos[X, ], combos[X, ], rho))),
+                    "prop" = prop)
         )
     }
